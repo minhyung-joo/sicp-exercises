e7ac24eb7b02c5713fda693c50acb62f728f6014
Minhyung Joo <minhyung@tiancai.pro>
2019-11-06 15:35:59 +0800

    exercise 1.16)

--- pascal.scm
+++ pascal.scm
@@ -1,9 +1,10 @@
 (define (p n)
-(define height (get-height n))
-(cond ((is-edge n height) 1)
-((< n 4) 1)
-(else (+ (p (- n height)) (p (+ 1 (- n height)))))
-)
+    (define height (get-height n))
+    (cond 
+        ((is-edge n height) 1)
+        ((< n 4) 1)
+        (else (+ (p (- n height)) (p (+ 1 (- n height)))))
+    )
 )
 
 (define (series n) (if (< n 2) n (+ n (series (- n 1))))
@@ -11,6 +12,12 @@
 (define (is-edge n h) (or (= n (+ 1 (series (- h 1)))) (= n (+ h (series (- h 1))))
 
 (define (get-height n)  
-(define (get-height-iter n h)
-(if (or (< n h) (= n h)) h (get-height-iter (- n h) (+ h 1))))
-(get-height-iter n 1))
+    (define (get-height-iter n h)
+        (if 
+            (or (< n h) (= n h)) 
+            h
+            (get-height-iter (- n h) (+ h 1))
+        )
+    )
+    (get-height-iter n 1)
+)
